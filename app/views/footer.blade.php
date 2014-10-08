689f578b0c1b64fc3a4dc54d8158d3e481853709
DanielSologuren <daniel.sologuren@gmail.com>
2014-10-07 22:34:58 -0400

    removed useless footer

--- app/views/footer.blade.php
+++ app/views/footer.blade.php
@@ -1,8 +1,3 @@
-<footer>
-	<p>
-		<small>Follow me <a href="http://twitter.com/dansologuren">@dansologuren</a></small>
-	</p>
-</footer>
 <script src="js/fastclick.js"></script>
 <script src="js/scroll.js"></script>
 <script src="js/fixed-responsive-nav.js"></script>
