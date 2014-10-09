99f87eddb5be2303dc9b45aa5288f47d9d0d03b9
danielsologuren <daniel.sologuren@gmail.com>
2014-10-09 16:18:43 -0400

    added missing viewport tag

--- app/views/layouts/base.blade.php
+++ app/views/layouts/base.blade.php
@@ -6,6 +6,7 @@
     @section('head')
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
     <script src="js/responsive-nav.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     @show
 </head>
 <body>
