2e2df45e7519ca6bf73b1e9d2115127a1225f7a5
DanielSologuren <daniel.sologuren@gmail.com>
2014-10-09 19:40:28 -0400

    Added google stuff

--- app/views/layouts/base.blade.php
+++ app/views/layouts/base.blade.php
@@ -6,6 +6,15 @@
     @section('head')
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
     <script src="js/responsive-nav.js"></script>
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-55618605-1', 'auto');
+  ga('send', 'pageview');
+  </script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @show
 </head>
