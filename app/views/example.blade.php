12db7f6f815f1fcc4038e7538d3c71a054ab98ac
DanielSologuren <daniel.sologuren@gmail.com>
2014-10-22 19:02:30 -0400

    centered all text

--- app/views/example.blade.php
+++ app/views/example.blade.php
@@ -2,10 +2,24 @@
 
 @section('body')
 	@include('header')
-	<section>
-      <h1>Hey!</h1>
-      <p>I'm Dan</p>
-      <p>I develop things in Drupal and some other stuff.</p>
+
+    <section id="home">
+      <h1>Hello!</h1>
+      <p>I'm a drupal dev based out in the DC area. I dove into development in 2009 and started working on Drupal in 2010.</p>
+      <p>Since then, I've worked on many Federal .gov projects from scope to deployment.</p>
     </section>
+
+    <section id="about">
+      <h1>About</h1>
+    </section>
+
+    <section id="projects">
+      <h1>Projects</h1>
+    </section>
+
+    <section id="blog">
+      <h1>Blog</h1>
+    </section>
+
     @include('footer')
 @stop
