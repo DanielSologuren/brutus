788d3f4cabd623a618ff5cb302dabaa8d7e08f2f
danielsologuren <daniel.sologuren@gmail.com>
2014-10-23 14:23:00 -0400

    added images and more content

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -7,11 +7,22 @@
       <h1>ABOUT</h1>
 
       <h3>Professional</h3>
-      <p>Drupal. Custom development and a little of front end</p>
+      <p>Most of my work is in Drupal, but primary focus is on PHP.</p>
+      <ul>
+      	<li>PHP -> Symfony, Laravel, Drupal CMS</li>
+      	<li>Font end ->Compass/Sass</li>
+      	<li>Stack -> Linux, Apache, Nginx, Mysql<li>
+      	<li>Things I dabble in -> Angularjs, Leaflet, Nodejs</li>
+      </ul>
       <p>I'm crazy for all things opensource and dabble in the ocasional mapping as a solution</p>
+      <p>Currently not as active as I should be in Drupal community, but more things should come along with D8 in beta and Symfony.</p>
+
+      <br/>
       <h3>Personal</h3>
+      <p>DC born. Maryland raised. Virginia living.</p>
       <p>I'm an avid swimmer who recently caught the triathlon bug.</p>
-      <p>Doing an Ironman is next on my list.</p>
+      <p>Doing an Ironman is next on my list!</p>
+
     </section>
 
     @include('footer')
--- app/views/work.blade.php
+++ app/views/work.blade.php
@@ -5,12 +5,23 @@
 	@include('menu')
 
 	<section id="work">
-		<h1>Work</h1><hr/>
-		<h2>In Drupal</h2>
+		<h1>Work</h1>
+		<img src="/images/ice.jpg">
 		<p>ice.gov</p>
+		<ul>
+			<li>Developed a sitewide mapping solution for ICE field offices and Detention Facility locator.
+			</li>
+		</ul>
+		<img src="/images/usda.jpg">
 		<p>fns.usda.gov</p>
+		<ul>
+			<li></li>
+		</ul>
+		<img src="/images/whatscooking.png">
 		<p>whatscooking.fns.usda.gov</p>
+		<img src="/images/ftc.jpg">
 		<p>ftc.gov</p>
+		<ul><li>Built prelimiary site architecture in content inventory for over 10k items in site redesign.<li></ul>
 	</section>
 
  @include('footer')
