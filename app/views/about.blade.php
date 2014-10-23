ddee1d940e38f56e383d5065bab460f425e598a4
danielsologuren <daniel.sologuren@gmail.com>
2014-10-23 14:25:37 -0400

    cleaned up spacing issues

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -8,15 +8,16 @@
 
       <h3>Professional</h3>
       <p>Most of my work is in Drupal, but primary focus is on PHP.</p>
+      <br/>
       <ul>
       	<li>PHP -> Symfony, Laravel, Drupal CMS</li>
       	<li>Font end ->Compass/Sass</li>
-      	<li>Stack -> Linux, Apache, Nginx, Mysql<li>
+      	<li>Stack -> Linux, Apache, Nginx, Mysql</li>
       	<li>Things I dabble in -> Angularjs, Leaflet, Nodejs</li>
       </ul>
+      <br/>
       <p>I'm crazy for all things opensource and dabble in the ocasional mapping as a solution</p>
       <p>Currently not as active as I should be in Drupal community, but more things should come along with D8 in beta and Symfony.</p>
-
       <br/>
       <h3>Personal</h3>
       <p>DC born. Maryland raised. Virginia living.</p>
