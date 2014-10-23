0a8fb1228cd77da7a4e9201e1c956ad26452223b
danielsologuren <daniel.sologuren@gmail.com>
2014-10-23 11:32:40 -0400

    updated contact and about page

---
+++ app/views/contact.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.base')
+
+@section('body')
+	@include('header')
+
+    <section id="contact">
+      <h1>Contact</h1>
+      <p>I think contact forms are lame.</p>
+      <p> Drop a line on <a href="https://www.linkedin.com/in/sologuren" target="_blank">Linkedin</a> or <a href="https://twitter.com/dansologuren" target="_blank">Twitter</a>.</p>
+    </section>
+
+    @include('footer')
+@stop
--- app/views/work.blade.php
+++ app/views/work.blade.php
@@ -4,14 +4,13 @@
 	@include('header')
 	@include('menu')
 
-<article>
-	 <h1>Work</h1>
-	<section>
+	<section id="work">
+		<h1>Work</h1>
 		<p>ice.gov</p>
 		<p>fns.usda.gov</p>
 		<p>whatscooking.fns.usda.gov</p>
 		<p>ftc.gov</p>
 	</section>
-</acticle>
+
  @include('footer')
 @stop
