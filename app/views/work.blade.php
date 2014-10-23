b92b0b4a0d21d65f4f50a3422cd4cce7868950ea
danielsologuren <daniel.sologuren@gmail.com>
2014-10-23 16:58:56 -0400

    removed bad html again

--- app/views/work.blade.php
+++ app/views/work.blade.php
@@ -7,18 +7,18 @@
 	<section id="work">
 		<h1>Work</h1>
 		<p>Immigration and Customs Enforcement<br/><a href="http://www.ice.gov/detention-facilities" target="_blank">http://www.ice.gov</a></p>
-		<small><a href="http://www.ice.gov/contact/field-offices" target="_blank">mapping</a>, site building, deployment, theming, custom development</small>
+		<p><small><a href="http://www.ice.gov/contact/field-offices" target="_blank">mapping</a>, site building, deployment, theming, custom development</small></p>
 		
         <br/>
 
 		<p>Food and Nutrition Services <br/> <a href="http://www.fns.usda.gov">http://www.fns.usda.gov</a></p>
-		<small>migration, site building, deployment, theming, custom development, multilingual</small>
+		<p><small>migration, site building, deployment, theming, custom development, multilingual</small></p>
 
 		<br/>
 		
         <p>USDA What's Cooking<br/>
             <a href="http://whatscooking.fns.usda.gov">http://whatscooking.fns.usda.gov</a></p>
-		<small>site building, deployment, theming, custom development, multilingual</small>
+		<p><small>site building, deployment, theming, custom development, multilingual</small></p>
 
 
 
