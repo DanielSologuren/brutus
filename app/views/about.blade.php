592070a75beffb339e7b65f8eabd356cca88c8a3
danielsologuren <daniel.sologuren@gmail.com>
2014-10-27 14:59:29 -0400

    updated about page

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -10,16 +10,16 @@
       <p>Most of my work is in Drupal, but primary focus is on PHP.</p>
       <br/>
       <ul>
-      	<li>PHP: Symfony, Laravel, Drupal CMS</li>
-      	<li>Font end: Compass/Sass</li>
-      	<li>Stack: Linux, Apache, Nginx, Mysql</li>
+      	<li>Symfony, Laravel, Drupal CMS</li>
+      	<li>Compass/Sass</li>
+      	<li>Linux, Apache, Nginx, Mysql</li>
       	<li>Things I dabble in: Angularjs, Leaflet</li>
       	<li>Things I <strong>want</strong> to dabble in: Packery, Nodejs, Hoodie</li>
       </ul>
       <br/>
       <p>I'm crazy for all things opensource.</p>
       <p>Currently not as active as I should be in Drupal community, but more things should come along with D8 in beta and Symfony.</p>
-      <br/>
+  <br/ style="border-bottom:outset #FFF;">
       <h3>Personal</h3>
       <p>DC born. Maryland raised. Virginia living.</p>
       <p>I'm an avid swimmer who recently caught the triathlon bug.</p>
