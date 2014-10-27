50d9eb2add260c17af0632fa239d03750b92b6a3
danielsologuren <daniel.sologuren@gmail.com>
2014-10-27 15:15:12 -0400

    updated work page

--- app/views/work.blade.php
+++ app/views/work.blade.php
@@ -6,14 +6,10 @@
 
 	<section id="work">
 		<h1>Work</h1>
-		<p>I'm currently with Blackstone Technology group doing great things in the Drupal + .gov world.</p>
-		<ul>
-			<li>USDA Food and Nutrition Services</li>
-			<li>DHS Immigration and Customs Enformcement</li>
-			<li>FTC</li>
-		</ul>
+		<p>I'm currently with Blackstone Technology group doing great things in the "Drupal + .gov" world.</p>
+		<p>I've had the opportunity to work on some amazing projects with the USDA, DHS, and FTC</p>
 		<p>Some cool things my team and I covered when building:</p>
-		<p><small><a href="http://www.ice.gov/contact/field-offices" target="_blank">mapping</a>, site building, deployment, theming, custom development, training</small></p>
+		<p><small><a href="http://www.ice.gov/contact/field-offices" target="_blank">mapping</a>, site building, migration (tough cookie), theming, custom development, training</small></p>
 	</section>
 
  @include('footer')
