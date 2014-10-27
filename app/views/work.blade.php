01671d31974d5b4ba1498779cb994a46295974dd
danielsologuren <daniel.sologuren@gmail.com>
2014-10-27 11:36:35 -0400

    updated work history

--- app/views/work.blade.php
+++ app/views/work.blade.php
@@ -6,22 +6,14 @@
 
 	<section id="work">
 		<h1>Work</h1>
-		<p>Immigration and Customs Enforcement<br/><a href="http://www.ice.gov/detention-facilities" target="_blank">http://www.ice.gov</a></p>
-		<p><small><a href="http://www.ice.gov/contact/field-offices" target="_blank">mapping</a>, site building, deployment, theming, custom development</small></p>
-		
-        <br/>
-
-		<p>Food and Nutrition Services <br/> <a href="http://www.fns.usda.gov">http://www.fns.usda.gov</a></p>
-		<p><small>migration, site building, deployment, theming, custom development, multilingual</small></p>
-
-		<br/>
-		
-        <p>USDA What's Cooking<br/>
-            <a href="http://whatscooking.fns.usda.gov">http://whatscooking.fns.usda.gov</a></p>
-		<p><small>site building, deployment, theming, custom development, multilingual</small></p>
-
-
-
+		<p>I'm currently with Blackstone Technology group doing great things in the Drupal + .gov world.</p>
+		<ul>
+			<li>USDA Food and Nutrition Services</li>
+			<li>DHS Immigration and Customs Enformcement</li>
+			<li>FTC</li>
+		</ul>
+		<p>Some cool things my team and I covered when building:</p>
+		<p><small><a href="http://www.ice.gov/contact/field-offices" target="_blank">mapping</a>, site building, deployment, theming, custom development, training</small></p>
 	</section>
 
  @include('footer')
