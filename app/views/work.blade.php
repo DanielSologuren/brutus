4c39e0686f7b252a54a08ca0658b46882691a4be
DanielSologuren <daniel.sologuren@gmail.com>
2015-01-10 15:53:22 -0500

    udpated work info

--- app/views/work.blade.php
+++ app/views/work.blade.php
@@ -6,8 +6,9 @@
 
 	<section id="work">
 		<h1>Work</h1>
-		<p>I'm currently with Blackstone Technology group doing great things in the "Drupal + .gov" world.</p>
-		<p>I've had the opportunity to work on some amazing projects with the USDA, DHS, and FTC</p>
+		<p>I work <a href="http://trellon.com">@Trellon</a></p>
+		<p>doing cool things in the Drupal.</p>
+		<p>I've also had the opportunity to work on some amazing projects in the ".gov" world.</p>
 		<p>Some cool things my team and I covered when building:</p>
 		<p><small><a href="http://www.ice.gov/contact/field-offices" target="_blank">mapping</a>, site building, migration (tough cookie), theming, custom development, training</small></p>
 	</section>
