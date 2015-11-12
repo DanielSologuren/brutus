f5b48a23fd851c0754e2d37684c2ee2042344550
DanielSologuren <daniel.sologuren@acquia.com>
2015-11-12 00:03:53 -0500

    update page

--- app/views/example.blade.php
+++ app/views/example.blade.php
@@ -6,8 +6,8 @@
     <section id="home">
       <h1>Hello!</h1>
       <p>My name is Dan.<p>
-      <p>I'm a DC based developer that builds with anything I can get my hands on.</p>
       <p>My primary focus is finding solutions to all web related things.</p>
+      <p>I'm a DC based developer that builds with anything I can get my hands on.</p>
 
     </section>
 
